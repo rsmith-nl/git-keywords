82802ed7a7df92d0b27adb1b5b3f5b87b7defd9e
Roland Smith <rsmith@xs4all.nl>
2012-06-29 23:32:43 +0200

    - Update checkfor() - Pet pylint.

--- kwclean.py
+++ kwclean.py
@@ -2,11 +2,13 @@
 # -*- coding: utf-8 -*-
 #
 # Author: R.F. Smith <rsmith@xs4all.nl>
-# Time-stamp: <2012-05-19 22:34:30 rsmith>
+# Time-stamp: <2012-06-29 23:25:24 rsmith>
 #
 # To the extent possible under law, Roland Smith has waived all copyright and
-# related or neighboring rights to dater. This work is published from the
+# related or neighboring rights to kwclean.py. This work is published from the
 # Netherlands. See http://creativecommons.org/publicdomain/zero/1.0/
+
+"""Remove the Date and Revision keyword contents from the standard input."""
 
 import sys
 import re
