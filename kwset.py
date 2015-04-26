a7ea1022b83359412ede0d176cbf23671f0b02db
Roland Smith <rsmith@xs4all.nl>
2015-04-26 16:46:55 +0200

    Reformatted Python scripts with yapf.
    With two exceptions;
    - Re-inserted empty line between file header and docstring.
    - Removed space before revision number slice.

--- kwset.py
+++ kwset.py
@@ -21,7 +21,7 @@ import sys
 def gitdate():
     """Get the date from the latest commit in ISO8601 format.
     """
-    args = ['git', 'log',  '-1', '--date=iso']
+    args = ['git', 'log', '-1', '--date=iso']
     outdata = subprocess.check_output(args, universal_newlines=True)
     outlines = outdata.splitlines()
     dline = [l for l in outlines if l.startswith('Date')]
@@ -36,10 +36,11 @@ def gitrev():
     """Get the latest tag and use it as the revision number. This presumes the
     habit of using numerical tags. Use the short hash if no tag available.
     """
-    args = ['git', 'describe',  '--tags', '--always']
+    args = ['git', 'describe', '--tags', '--always']
     try:
         with open(os.devnull, 'w') as bb:
-            r = subprocess.check_output(args, stderr=bb,
+            r = subprocess.check_output(args,
+                                        stderr=bb,
                                         universal_newlines=True)[:-1]
     except subprocess.CalledProcessError:
         return ''.join(['$', 'Revision', '$'])
@@ -52,7 +53,7 @@ def main():
     dre = re.compile(''.join([r'\$', r'Date:?\$']))
     rre = re.compile(''.join([r'\$', r'Revision:?\$']))
     currp = os.getcwd()
-    if not os.path.exists(currp+'/.git'):
+    if not os.path.exists(currp + '/.git'):
         print >> sys.stderr, 'This directory is not controlled by git!'
         sys.exit(1)
     date = gitdate()
