9bdd58649cf99c30d3ca77014245acebb955c775
Roland Smith <rsmith@xs4all.nl>
2015-09-23 22:14:50 +0200

    Use subprocess.DEVNULL.
    Also reformat docstrings and put the “main” function in the beginning of the
    file.

--- update-all-keywords.py
+++ update-all-keywords.py
@@ -2,7 +2,7 @@
 # vim:fileencoding=utf-8:ft=python
 #
 # Author: R.F. Smith <rsmith@xs4all.nl>
-# Last modified: 2015-05-03 22:41:41 +0200
+# Last modified: 2015-09-23 21:17:05 +0200
 #
 # To the extent possible under law, Roland Smith has waived all copyright and
 # related or neighboring rights to update-all-keywords.py. This work is
@@ -19,73 +19,11 @@ import subprocess
 import sys
 
 
-def checkfor(args):
-    """Make sure that a program necessary for using this script is
-    available.
-
-    Arguments:
-    args -- string or list of strings of commands. A single string may
-            not contain spaces.
-    """
-    if isinstance(args, str):
-        if ' ' in args:
-            raise ValueError('No spaces in single command allowed.')
-        args = [args]
-    try:
-        with open(os.devnull, 'w') as bb:
-            subprocess.check_call(args, stdout=bb, stderr=bb)
-    except subprocess.CalledProcessError:
-        print("Required program '{}' not found! exiting.".format(args[0]))
-        sys.exit(1)
-
-
-def git_ls_files():
-    """Find ordinary files that are controlled by git.
-
-    :returns: A list of files
-    """
-    args = ['git', 'ls-files']
-    flist = subprocess.check_output(args).splitlines()
-    return flist
-
-
-def git_not_checkedin():
-    """Find files that are modified but are not checked in.
-
-    :returns: A list of modified files that are not checked in.
-    """
-    lns = subprocess.check_output(['git', 'status', '-s']).splitlines()
-    lns = [l.split()[-1] for l in lns]
-    return lns
-
-
-def keywordfiles(fns):
-    """Filter those files that have keywords in them
-
-    :fns: A list of filenames
-    :returns: A list for filenames for files that contain keywords.
-    """
-    # These lines are encoded otherwise they would be mangled if this file
-    # is checked in!
-    datekw = b64decode('JERhdGU=')
-    revkw = b64decode('JFJldmlzaW9u')
-    rv = []
-    for fn in fns:
-        with open(fn, 'rb') as f:
-            try:
-                mm = mmap.mmap(f.fileno(), 0, access=mmap.ACCESS_READ)
-                if mm.find(datekw) > -1 or mm.find(revkw) > -1:
-                    rv.append(fn)
-                mm.close()
-            except ValueError:
-                pass
-    return rv
-
-
 def main(args):
     """Main program.
 
-    :args: command line arguments
+    Arguments:
+        args: command line arguments
     """
     # Check if git is available.
     checkfor(['git', '--version'])
@@ -109,10 +47,79 @@ def main(args):
         print('{}: Updating all files.'.format(args[0]))
         for fn in kwfn:
             os.remove(fn)
-        args = ['git', 'checkout', '-f'] + kwfn
-        subprocess.call(args)
+        sargs = ['git', 'checkout', '-f'] + kwfn
+        subprocess.call(sargs)
     else:
         print('{}: Nothing to update.'.format(args[0]))
+
+
+def checkfor(args):
+    """Make sure that a program necessary for using this script is
+    available.
+
+    Arguments:
+        args: String or list of strings of commands. A single string may
+              not contain spaces.
+    """
+    if isinstance(args, str):
+        if ' ' in args:
+            raise ValueError('No spaces in single command allowed.')
+        args = [args]
+    try:
+        subprocess.check_call(args, stdout=subprocess.DEVNULL,
+                              stderr=subprocess.DEVNULL)
+    except subprocess.CalledProcessError:
+        print("Required program '{}' not found! exiting.".format(args[0]))
+        sys.exit(1)
+
+
+def git_ls_files():
+    """Find ordinary files that are controlled by git.
+
+    Returns:
+        A list of files
+    """
+    args = ['git', 'ls-files']
+    flist = subprocess.check_output(args).decode('utf8').splitlines()
+    return flist
+
+
+def git_not_checkedin():
+    """Find files that are modified but are not checked in.
+
+    Returns:
+        A list of modified files that are not checked in.
+    """
+    lns = subprocess.check_output(['git', 'status', '-s'])
+    lns.decode('utf8').splitlines()
+    lns = [l.split()[-1] for l in lns]
+    return lns
+
+
+def keywordfiles(fns):
+    """Filter those files that have keywords in them
+
+    Arguments:
+        fns: A list of filenames.
+
+    Returns:
+        A list for filenames for files that contain keywords.
+    """
+    # These lines are encoded otherwise they would be mangled if this file
+    # is checked in!
+    datekw = b64decode('JERhdGU=')
+    revkw = b64decode('JFJldmlzaW9u')
+    rv = []
+    for fn in fns:
+        with open(fn, 'rb') as f:
+            try:
+                mm = mmap.mmap(f.fileno(), 0, access=mmap.ACCESS_READ)
+                if mm.find(datekw) > -1 or mm.find(revkw) > -1:
+                    rv.append(fn)
+                mm.close()
+            except ValueError:
+                pass
+    return rv
 
 
 if __name__ == '__main__':
--- update-modified-keywords.py
+++ update-modified-keywords.py
@@ -2,7 +2,7 @@
 # vim:fileencoding=utf-8:ft=python
 #
 # Author: R.F. Smith <rsmith@xs4all.nl>
-# Last modified: 2015-05-03 22:43:37 +0200
+# Last modified: 2015-09-23 21:19:02 +0200
 #
 # To the extent possible under law, Roland Smith has waived all copyright and
 # related or neighboring rights to update-modified-keywords.py. This work is
@@ -19,77 +19,11 @@ import subprocess
 import sys
 
 
-def checkfor(args):
-    """Make sure that a program necessary for using this script is
-    available.
-
-    Arguments:
-    args -- string or list of strings of commands. A single string may
-            not contain spaces.
-    """
-    if isinstance(args, str):
-        if ' ' in args:
-            raise ValueError('No spaces in single command allowed.')
-        args = [args]
-    try:
-        with open(os.devnull, 'w') as bb:
-            subprocess.check_call(args, stdout=bb, stderr=bb)
-    except subprocess.CalledProcessError:
-        print("Required program '{}' not found! exiting.".format(args[0]))
-        sys.exit(1)
-
-
-def modifiedfiles():
-    """Find files that have been modified in the last commit.
-
-    :returns: A list of filenames.
-    """
-    fnl = []
-    try:
-        args = ['git', 'diff-tree', 'HEAD~1', 'HEAD', '--name-only', '-r',
-                '--diff-filter=ACMRT']
-        with open(os.devnull, 'w') as bb:
-            fnl = subprocess.check_output(args, stderr=bb).splitlines()
-            # Deal with unmodified repositories
-            if len(fnl) == 1 and fnl[0] is 'clean':
-                return []
-    except subprocess.CalledProcessError as e:
-        if e.returncode == 128:  # new repository
-            args = ['git', 'ls-files']
-            with open(os.devnull, 'w') as bb:
-                fnl = subprocess.check_output(args, stderr=bb).splitlines()
-    # Only return regular files.
-    fnl = [i for i in fnl if os.path.isfile(i)]
-    return fnl
-
-
-def keywordfiles(fns):
-    """Filter those files that have keywords in them
-
-    :fns: A list of filenames
-    :returns: A list for filenames for files that contain keywords.
-    """
-    # These lines are encoded otherwise they would be mangled if this file
-    # is checked in my git repo!
-    datekw = b64decode('JERhdGU=')
-    revkw = b64decode('JFJldmlzaW9u')
-    rv = []
-    for fn in fns:
-        with open(fn, 'rb') as f:
-            try:
-                mm = mmap.mmap(f.fileno(), 0, access=mmap.ACCESS_READ)
-                if mm.find(datekw) > -1 or mm.find(revkw) > -1:
-                    rv.append(fn)
-                mm.close()
-            except ValueError:
-                pass
-    return rv
-
-
 def main(args):
     """Main program.
 
-    :args: command line arguments
+    Arguments:
+        args: command line arguments
     """
     # Check if git is available.
     checkfor(['git', '--version'])
@@ -111,8 +45,79 @@ def main(args):
         sys.exit(0)
     for fn in kwfn:
         os.remove(fn)
-    args = ['git', 'checkout', '-f'] + kwfn
-    subprocess.call(args)
+    sargs = ['git', 'checkout', '-f'] + kwfn
+    subprocess.call(sargs)
+
+
+def checkfor(args):
+    """Make sure that a program necessary for using this script is
+    available.
+
+    Arguments:
+    args -- string or list of strings of commands. A single string may
+            not contain spaces.
+    """
+    if isinstance(args, str):
+        if ' ' in args:
+            raise ValueError('No spaces in single command allowed.')
+        args = [args]
+    try:
+        subprocess.check_call(args, stdout=subprocess.DEVNULL,
+                              stderr=subprocess.DEVNULL)
+    except subprocess.CalledProcessError:
+        print("Required program '{}' not found! exiting.".format(args[0]))
+        sys.exit(1)
+
+
+def modifiedfiles():
+    """Find files that have been modified in the last commit.
+
+    Returns:
+        A list of filenames.
+    """
+    fnl = []
+    try:
+        args = ['git', 'diff-tree', 'HEAD~1', 'HEAD', '--name-only', '-r',
+                '--diff-filter=ACMRT']
+        fnl = subprocess.check_output(args, stderr=subprocess.DEVNULL)
+        fnl = fnl.decode('utf8').splitlines()
+        # Deal with unmodified repositories
+        if len(fnl) == 1 and fnl[0] is 'clean':
+            return []
+    except subprocess.CalledProcessError as e:
+        if e.returncode == 128:  # new repository
+            args = ['git', 'ls-files']
+            fnl = subprocess.check_output(args, stderr=subprocess.DEVNULL)
+            fnl = fnl.decode('utf8').splitlines()
+    # Only return regular files.
+    fnl = [i for i in fnl if os.path.isfile(i)]
+    return fnl
+
+
+def keywordfiles(fns):
+    """Filter those files that have keywords in them
+
+    Arguments:
+        fns: A list of filenames.
+
+    Returns:
+        A list for filenames for files that contain keywords.
+    """
+    # These lines are encoded otherwise they would be mangled if this file
+    # is checked in my git repo!
+    datekw = b64decode('JERhdGU=')
+    revkw = b64decode('JFJldmlzaW9u')
+    rv = []
+    for fn in fns:
+        with open(fn, 'rb') as f:
+            try:
+                mm = mmap.mmap(f.fileno(), 0, access=mmap.ACCESS_READ)
+                if mm.find(datekw) > -1 or mm.find(revkw) > -1:
+                    rv.append(fn)
+                mm.close()
+            except ValueError:
+                pass
+    return rv
 
 
 if __name__ == '__main__':
